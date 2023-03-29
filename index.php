093fde50e25c0e0f29cf6e4619582f39b4b4cdf7
Ksenya <ksenya.nikitchenko.03@mail.ru>
2023-03-29 09:01:56 +0300

    Форма

--- index.php
+++ index.php
@@ -1,83 +1,77 @@
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-  <meta charset="utf-8">
-  <title>Человеческие сверхспособности</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-  <header>
-    <div id="название">
-      <h1>Человеческие сверхспособности</h1>
-    </div>
-  </header>
-  <div class="form">
-    <form action="/" method="POST">
-      <div>
-        <label>
-          Введите имя:<br />
-          <input name="name" placeholder="Введите имя" />
-        </label>
-      </div>
-      <div>
-        <label>
-          Адрес электронной почты:<br />
-          <input name="email" type="email" placeholder="Введите email" />
-        </label>
-      </div>
-      <div>
-        <label for="year">Год рождения</label>
-			<select id="year" name="year">
-				<option selected>Год</option>
-    <?php
-        for ($i = 1980; $i <= 2014; $i++)
-            echo '<option>' . $i . '</option>';
-    ?>
-			</select>
-      </div>
-      <div>
-        Выберите пол:<br />
-        <label><input type="radio" checked="checked" name="gender" value="female" />
-          Женский</label>
-        <label><input type="radio" name="gender" value="male" />
-          Мужской</label>
-      </div>
-      <div>
-        Количество конечностей:<br />
-        <label><input type="radio" checked="checked" name="limbs" value="1" />
-          1</label>
-        <label><input type="radio" name="limbs" value="2" />
-          2</label>
-        <label><input type="radio" name="limbs" value="3" />
-          3</label>
-        <label><input type="radio" name="limbs" value="4" />
-          4</label>
-        <label>
-      </div>
-      <div>
-        Сверхспособности:<br>
-        <select name="superpowers[]" multiple="multiple">
-          <option value="deathless">Бессмертие</option>
-          <option value="walls" selected="selected">Прохождение сквозь стены</option>
-          <option value="levitation" selected="selected">Левитация</option>
-          <option value="deathless">Управление стихиями</option>
-          <option value="deathless">Путешествие во времени</option>
-        </select>
-      </div>
-      <div>
-        Биография:<br>
-        <textarea name="biography">Напишите о себе</textarea>
-        <br><label><input type="checkbox" checked="checked" name="check-1" />
-          с контрактом ознакомлен(а)</label>
-      </div>
-      <div>
-        <input type="submit" class="submit" value="Отправить" />
-      </div>
-    </form>
-  </div>
-
-</body>
-
-</html>
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+// В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+    if (!empty($_GET['save'])) {// Если есть параметр save, то выводим сообщение пользователю.
+        print('Спасибо, результаты сохранены.');
+    }
+    include('form.php');
+    exit();
+}
+// Проверяем ошибки.
+$errors = FALSE;
+if (empty($_POST['name'])) {
+    print('Введите имя<br>');
+    $errors = TRUE;
+}
+if (empty($_POST['email']) or !(strpos($_POST['email'], '@'))) {
+    print('Введите e-mail<br>');
+    $errors = TRUE;
+}
+if (empty($_POST['year'])) {
+    print('Выберите из списка год рождения<br>');
+    $errors = TRUE;
+}
+if (empty($_POST['gender'])) {
+    print('Укажите ваш пол<br>');
+    $errors = TRUE;
+}
+if (empty($_POST['limbs'])){
+    print ('Выберите количество конечностей<br>');
+    $errors = true;
+}
+if (empty($_POST['superpowers'])){
+    print ('Выберите минимум одну сверхспособность<br>');
+    $errors = true;
+}
+else {
+    $super = serialize($_POST['super']);
+}
+if (empty($_POST['biography'])){
+    print ('Расскажите о себе<br>');
+    $errors = true;
+}
+if (empty($_POST['check-kontrol'])){
+    print ('Обязательно ознакомьтесь с контрактом перед отправкой формы<br>');
+    $errors = true;
+}
+if ($errors) {// При наличии ошибок завершаем работу скрипта.
+    exit();
+}
+// Сохранение в базу данных.
+$user = 'u52984';
+$pass = '8295850';
+$db = new PDO('mysql:host=localhost;dbname=u47567', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+// Подготовленный запрос. Не именованные метки.
+try {
+    $stmt = $db->prepare("INSERT INTO person SET name = ?, email = ?, year = ?, gender = ?, limbs = ?, biography = ?");
+        $stmt -> execute(array(
+        $_POST['name'],
+        $_POST['email'],
+        $_POST['year'],
+        $_POST['gender'],
+        $_POST['limbs'],
+        $_POST['biography'],
+    ));
+    $stmt = $db->prepare("INSERT INTO superpower SET name = ?, superpower = ?");
+        $stmt -> execute(array(
+        $_POST['name'], 
+        $_POST['super'] = implode(', ', $_POST['super']),
+    ));
+}
+catch(PDOException $e){
+print('Error: ' . $e->getMessage());
+exit();
+}
+// stmt - это "дескриптор состояния"
+header('Location: ?save=1');
